3010f264bb97c10ea2ac1f2d9985eaa666d96b00
Amar Octagon <amar@octagoninfotech.com>
2023-03-09 20:43:16 +0545

    init: section 14

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -27,6 +27,79 @@ it("returns a 401 if the user is not authenticated", async () => {
     .expect(401);
 });
 
-it("returns a 401 if the user does not own the tickets", async () => {});
+it("returns a 401 if the user does not own the tickets", async () => {
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", await global.signin())
+    .send({
+      title: "adfgsasfasdf",
+      price: 30,
+    });
 
-it("returns a 400 if the user provided invalid title or price", async () => {});
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", await global.signin())
+    .send({
+      title: "asfasdf",
+      price: 10,
+    })
+    .expect(401);
+});
+
+it("returns a 400 if the user provided invalid title or price", async () => {
+  const cookie = await global.signin();
+
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", cookie)
+    .send({
+      title: "fasfasdf",
+      price: 20,
+    });
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", cookie)
+    .send({
+      title: "",
+      price: 20,
+    })
+    .expect(400);
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", cookie)
+    .send({
+      title: "fasfsadf",
+      price: -20,
+    })
+    .expect(400);
+});
+
+it("updates the ticket provided valid inputs", async () => {
+  const cookie = await global.signin();
+
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", cookie)
+    .send({
+      title: "safasdf",
+      price: 20,
+    });
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", cookie)
+    .send({
+      title: "New Title",
+      price: 200,
+    })
+    .expect(200);
+
+  const ticketRes = await request(app)
+    .get(`/api/tickets/${response.body.id}`)
+    .send();
+
+  expect(ticketRes.body.title).toEqual("New Title");
+  expect(ticketRes.body.price).toEqual(200);
+});
