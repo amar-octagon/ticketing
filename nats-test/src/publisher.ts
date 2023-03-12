5ab81dfa47982964a16e58d2c5bab5d1080142c6
Amar Octagon <amar@octagoninfotech.com>
2023-03-12 18:04:19 +0545

    episode 306

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,9 +1,19 @@
 import nats from "node-nats-streaming";
 
 const stan = nats.connect("ticketing", "abc", {
-  url: "http://localhost:422",
+  url: "http://localhost:4222",
 });
 
 stan.on("connect", () => {
   console.log("Publisher connected to NATS");
+
+  const data = {
+    id: "123",
+    title: "concert",
+    price: 20,
+  };
+
+  stan.publish("ticket:created", JSON.stringify(data), () => {
+    console.log("Event published");
+  });
 });
