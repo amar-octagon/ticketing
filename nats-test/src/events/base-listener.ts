9ea9b335704e7ec983e702284a6a4de50de54223
Amar Octagon <amar@octagoninfotech.com>
2023-03-19 11:37:03 +0545

    section:15 episode 326

--- nats-test/src/events/base-listener.ts
+++ nats-test/src/events/base-listener.ts
@@ -1,9 +1,15 @@
 import { Message, Stan } from "node-nats-streaming";
+import { Subjects } from "./subjects";
 
-export abstract class Listener {
-  abstract subject: string;
+interface Event {
+  subject: Subjects;
+  data: any;
+}
+
+export abstract class Listener<T extends Event> {
+  abstract subject: T["subject"];
   abstract queueGroupName: string;
-  abstract onMessage(data: any, msg: Message): void;
+  abstract onMessage(data: T["data"], msg: Message): void;
   private client: Stan;
   protected ackWait = 5 * 1000;
 
@@ -37,6 +43,8 @@ export abstract class Listener {
 
   parseMessage(msg: Message) {
     const data = msg.getData();
-    return typeof data === "string" ? JSON.parse(data) : JSON.parse(data.toString("utf-8"));
+    return typeof data === "string"
+      ? JSON.parse(data)
+      : JSON.parse(data.toString("utf-8"));
   }
 }
