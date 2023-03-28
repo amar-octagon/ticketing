e96ce38c24779d154198169c7f8ddd824f52899b
Amar Mandal <amarmandal2073@gmail.com>
2023-03-28 18:54:38 +0545

    updates

--- common/build/events/base-listener.d.ts
+++ common/build/events/base-listener.d.ts
@@ -11,7 +11,7 @@ export declare abstract class Listener<T extends Event> {
     private client;
     protected ackWait: number;
     constructor(client: Stan);
-    subscriptionOption(): any;
+    subscriptionOption(): import("node-nats-streaming").SubscriptionOptions;
     listen(): void;
     parseMessage(msg: Message): any;
 }
--- common/build/middlewares/require-auth.d.ts
+++ common/build/middlewares/require-auth.d.ts
@@ -1,1 +1,2 @@
+import { Request, Response, NextFunction } from "express";
 export declare const requireAuth: (req: Request, rese: Response, next: NextFunction) => void;
