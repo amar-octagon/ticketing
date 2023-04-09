81888ffe5a4064bd8ba9f66fb6527986b67fcae5
Amar Octagon <amar@octagoninfotech.com>
2023-04-09 14:25:31 +0545

    'updates'

--- common/build/events/types/order-status.d.ts
+++ common/build/events/types/order-status.d.ts
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 export declare enum OrderStatus {
     Created = "created",
     Cancelled = "cancelled",
     AwaitingPayment = "awaiting:payment",
     Complete = "complete"
 }
-=======
-export declare enum OrderStatus {
-    Created = "created",
-    Cancelled = "cancelled",
-    AwaitingPayment = "awaiting:payment",
-    Complete = "complete"
-}
->>>>>>> 9e80ec38869d84def7dd15bd1cd674018ce4b4b4
--- common/build/events/types/order-status.js
+++ common/build/events/types/order-status.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.OrderStatus = void 0;
@@ -14,23 +13,3 @@ var OrderStatus;
     OrderStatus["AwaitingPayment"] = "awaiting:payment";
     OrderStatus["Complete"] = "complete";
 })(OrderStatus = exports.OrderStatus || (exports.OrderStatus = {}));
-=======
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.OrderStatus = void 0;
-var OrderStatus;
-(function (OrderStatus) {
-    // When the order has been created, but the
-    // ticket it is trying to order has not been reserved
-    OrderStatus["Created"] = "created";
-    // The ticket the order is trying to reserve has already
-    // been reserved, or when the user has cancelled the order.
-    // The order expires before payment
-    OrderStatus["Cancelled"] = "cancelled";
-    // The order has successfully reserved the ticket
-    OrderStatus["AwaitingPayment"] = "awaiting:payment";
-    // The order has reserved the ticket and the user has
-    // provided payment successfully
-    OrderStatus["Complete"] = "complete";
-})(OrderStatus = exports.OrderStatus || (exports.OrderStatus = {}));
->>>>>>> 9e80ec38869d84def7dd15bd1cd674018ce4b4b4
